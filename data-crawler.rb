6876c5bde3b5288792a30826c2285be2bcd4c116
Colin Dean <colindean@us.ibm.com>
2016-07-01 11:42:16 -0400

    Removes version and hash from data-crawler
    Rolling releases means this will never be up to date.

--- data-crawler.rb
+++ data-crawler.rb
@@ -2,8 +2,6 @@ class DataCrawler < Formula
   desc "A crawler that connects data repositories to Watson Developer Cloud services"
   homepage "https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/"
   url "http://ibm.biz/watson-crawler-zip"
-  version "0.3.2"
-  sha256 "644d5e06f05c7ca02bdd0902fccc36700f3f15da3cc40b12bd9d48d7dfb0d937"
 
   def install
     rm Dir["bin/*.bat"] # unused on osx
