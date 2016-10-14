1f2d075fe3e918ef73457701b4a4647e31a31a61
Bruce Adams <ba@us.ibm.com>
2016-10-14 16:57:12 -0400

    Upgrade to `kale` 1.8.0 (#5)

--- kale.rb
+++ kale.rb
@@ -1,8 +1,8 @@
 class Kale < Formula
   desc "Provisions & configures WDC Retrieve & Rank, Document Conversion services"
   homepage "https://github.com/IBM-Watson/kale"
-  sha256 "09c8aa86cacd8acc4403e9ffcadda1851a884718d86162cdb00ea48987b25c8c"
-  url "https://github.com/IBM-Watson/kale/releases/download/v1.7.0/kale-1.7.0-standalone.jar"
+  sha256 "81e0aade9a7dc2ff52fae6a0cff69dbbc811c3a935cf9f0587852c3426eefff1"
+  url "https://github.com/IBM-Watson/kale/releases/download/v1.8.0/kale-1.8.0-standalone.jar"
 
   depends_on :java
 
