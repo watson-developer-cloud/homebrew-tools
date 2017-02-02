3a9524b5b132e21e74681f42344f32013edbba05
Colin A. Dean <colindean@us.ibm.com>
2017-02-01 21:23:29 -0500

    Adds version and sha256 to data-crawler
    Since we now calculate this, it should be easier to automate...

--- data-crawler.rb
+++ data-crawler.rb
@@ -2,6 +2,8 @@ class DataCrawler < Formula
   desc "A crawler that connects data repositories to Watson Developer Cloud services"
   homepage "https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/"
   url "http://ibm.biz/watson-crawler-zip"
+  version "1.3.0"
+  sha256 "47181a13e7851a2a2e4085b4acf6cbb7398bccdca8b0cb678c1db15402ceffb0"
 
   def install
     rm Dir["bin/*.bat"] # unused on osx
