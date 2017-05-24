14378c9a2e29ed05f2d9b618f734ee63c9f4dc89
Bruce Adams <ba@us.ibm.com>
2017-05-24 19:34:59 -0400

    Update to watson-discovery-tool 2.2.7

--- watson-discovery-tool.rb
+++ watson-discovery-tool.rb
@@ -1,8 +1,8 @@
 class WatsonDiscoveryTool < Formula
   desc "Command-line interface to the IBM Watson Discovery Service API"
   homepage "https://github.com/bruceadams/wdscli"
-  url "https://github.com/bruceadams/wdscli/releases/download/2.2.6/wdscli.macos"
-  sha256 "09ebd59f5c90fcaccacd73154a4e481234da6b74c55c5813a777744e54357a7f"
+  url "https://github.com/bruceadams/wdscli/releases/download/2.2.7/wdscli.macos"
+  sha256 "4e293fd3ec102f0f0e522d8ba10aaba3b29223c83bbf51ade559b0c7b2cccfe1"
 
   def install
     bin.install "wdscli.macos" => "watson-discovery-tool"
@@ -15,7 +15,7 @@ class WatsonDiscoveryTool < Formula
     fish_completion.install "watson-discovery-tool.fish"
 
     system "#{bin}/watson-discovery-tool generate-completions zsh  > watson-discovery-tool.zsh"
-    zsh_completion.install  "watson-discovery-tool.zsh"
+    zsh_completion.install "watson-discovery-tool.zsh"
   end
 
   test do
