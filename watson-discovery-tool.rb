b328e424482fcb029efac181169e610129488caf
Bruce Adams <ba@us.ibm.com>
2017-06-21 14:21:05 -0400

    Update watson-discovery-tool to 2.3.0

--- watson-discovery-tool.rb
+++ watson-discovery-tool.rb
@@ -1,8 +1,8 @@
 class WatsonDiscoveryTool < Formula
   desc "Command-line interface to the IBM Watson Discovery Service API"
   homepage "https://github.com/bruceadams/wdscli"
-  url "https://github.com/bruceadams/wdscli/releases/download/2.2.8/wdscli.macos"
-  sha256 "026c992e9782e7c232251a12abe7ea02f67640e1fd00ac6f89d010fea46b273c"
+  url "https://github.com/bruceadams/wdscli/releases/download/2.3.0/wdscli.macos"
+  sha256 "0cced55d02c231628575ffb259dec10a2b8e7591c9775a498bcc3de568f48732"
 
   def install
     bin.install "wdscli.macos" => "watson-discovery-tool"
